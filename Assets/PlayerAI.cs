eb1050ec3f9902c578d70a5b0c5c4cab54eef532
Davide Oggioni <33121159+daviogg@users.noreply.github.com>
2018-06-07 17:59:51 +0200

    Add pathfinder package and script for PlayerAI

---
+++ Assets/PlayerAI.cs
@@ -0,0 +1,106 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
+public class PlayerAI : MonoBehaviour {
+
+    public Transform target;
+
+    public float updateRate = 2f;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    public Path path;
+
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    private int currentWaypoint = 0;
+
+    [HideInInspector]
+    public bool pathIsEnded = false;
+
+    public float nextWaypointDistance = 3;
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        if(target == null)
+        {
+            Debug.LogError("No player found");
+            return;
+        }
+        StartCoroutine(UpdatePath());
+    }
+
+    IEnumerator UpdatePath()
+    {
+        if(target == null)
+        {
+            yield return false;
+        }
+
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        yield return new WaitForSeconds(1f / updateRate);
+        StartCoroutine(UpdatePath());
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Any Error? "  + p.error);
+
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        if (path == null)
+            return;
+
+        if(currentWaypoint>= path.vectorPath.Count)
+        {
+            if (pathIsEnded)
+                return;
+
+            Debug.Log("End of path reached");
+            pathIsEnded = true;
+            return;
+
+        }
+
+        pathIsEnded = false;
+
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed * Time.fixedDeltaTime;
+
+        rb.AddForce(dir, fMode);
+
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+
+        if (dist < nextWaypointDistance)
+        {
+            currentWaypoint++;
+            return;
+        }
+    }
+
+
+
+
+}
