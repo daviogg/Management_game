7522e5c5f14872ccbb408ae1b78ddcc4b5df17bb
Luca Gazzardi <luca.gazzardi@reti.it>
2018-06-07 17:49:57 +0200

    Added Computer and Developer scripts

---
+++ Assets/Developer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Developer : MonoBehaviour {
+
+    [HideInInspector]
+    public string Name;
+
+    private bool hasProject = false;
+
+	// Use this for initialization
+	void Start () {
+        Name = "Gianfernando";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AssignProject() //
+    {
+        if (!hasProject)
+        {
+            hasProject = true;
+            Debug.Log("Project assigned to " + Name);
+        }
+    }
+}
---
+++ Assets/Scripts/Computer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Computer : MonoBehaviour {
+
+    public Canvas AssignMenu;
+
+    private bool isAssigned = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnMouseDown()
+    {        
+        AssignMenu.enabled = !AssignMenu.enabled;
+        Debug.Log("Ciaodiaocao");
+    }
+
+    public void AssignLocation()
+    {
+        if (isAssigned)
+        {
+            return;
+        }
+
+        // get active developer
+        // assign this to him
+        isAssigned = true;
+        Debug.Log("Assigned");
+    }
+
+   
+}
