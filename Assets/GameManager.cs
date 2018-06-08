7a75dc92c6859fb486e2f92a6c196ca43156e85a
Luca Gazzardi <luca.gazzardi@reti.it>
2018-06-08 17:44:49 +0200

    Added Developer, GameManager, Location behaviour

---
+++ Assets/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager GameController;
+
+    public GameObject CurrentActive; //Active developer container
+    public GameObject DeveloperPool;
+
+    [HideInInspector]
+    public Developer ActiveDeveloper;
+
+    // Use this for initialization
+    void Start () {
+        GameController = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ActiveClickedDeveloper(Developer toActivate) //Set the clicked Developer as active
+    {
+        toActivate.transform.parent = CurrentActive.transform;        
+        ActiveDeveloper = toActivate;
+
+        ActiveDeveloper.GetComponentsInChildren<SpriteRenderer>()[1].enabled = true;
+    }
+
+    public void DeactiveOldDeveloper() //Set the clicked Developer as active
+    {        
+        if (ActiveDeveloper != null)
+        {
+            ActiveDeveloper.GetComponentsInChildren<SpriteRenderer>()[1].enabled = false;
+            ActiveDeveloper.transform.parent = DeveloperPool.transform;
+        }
+    }
+}
---
+++ Assets/Scripts/Developer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Developer : MonoBehaviour {
+
+    [HideInInspector]
+    public string Name;
+    [HideInInspector]
+    public Project Project { get; set; }
+    
+    public TextMesh TextName;
+        
+    private bool hasProject = false;    
+
+	// Use this for initialization
+	void Start () {
+        Name = Names.GetRandomName();
+        gameObject.name = Name;
+        TextName.text = Name;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void AssignProject() //Assign this developer to a project
+    {
+        if (!hasProject)
+        {
+            // Project = 
+            hasProject = true;
+            Debug.Log("Project assigned to " + Name);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GameManager.GameController.DeactiveOldDeveloper();
+        GameManager.GameController.ActiveClickedDeveloper(this);
+    }
+}
